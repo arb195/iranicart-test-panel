8c5453c83cf8e036dc348ea44f8f9fc38c5fff35
Paliz <alirezabagheri195@gmail.com>
2024-11-30 17:00:02 +0330

    last commit in day

--- src/components/singeTicket/singeTicket.tsx
+++ src/components/singeTicket/singeTicket.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/router";
+
 export default function SingeTicket() {
-  return <div>msg</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>{id ? <p>Showing details for ticket ID: {id}</p> : <p>Loading...</p>}</>
+  );
 }
